054304247af63eae8b492fe7f06cddfe1ebeed9e
Pedro <ing.pedro.theran@gmail.com>
2018-07-14 12:22:49 -0500

    Commit inicial proyecto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,34 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  numberA;
+  numberB;
+  typeOfAction;
+  result;
 
   constructor(public navCtrl: NavController) {
 
   }
+  performFunction(type){
+    var a = parseInt(this.numberA);
+    var b = parseInt(this.numberB);
+    console.log(type);
+
+    if( type == 's'){
+      this.typeOfAction = "Suma"
+      this.result = a+b
+    }else if(type == 'r'){
+      this.typeOfAction = "Resta"
+      this.result = a - b;
+    }else if(type == 'm'){
+      this.typeOfAction = "Multiplicacion"
+      this.result = a *b
+    }else if(type == 'd'){
+      this.typeOfAction = "Division"
+      this.result = a/b
+    }
+
+  }
+
 
 }
