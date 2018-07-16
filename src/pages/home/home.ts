82e5792522dfbe95eefc2fba67a7d6ba488d35b5
Pedro <ing.pedro.theran@gmail.com>
2018-07-16 00:51:08 -0500

    Comit v2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,33 +6,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  numberA;
-  numberB;
-  typeOfAction;
+  
   result;
+  
+  
 
   constructor(public navCtrl: NavController) {
 
   }
-  performFunction(type){
-    var a = parseInt(this.numberA);
-    var b = parseInt(this.numberB);
-    console.log(type);
-
-    if( type == 's'){
-      this.typeOfAction = "Suma"
-      this.result = a+b
-    }else if(type == 'r'){
-      this.typeOfAction = "Resta"
-      this.result = a - b;
-    }else if(type == 'm'){
-      this.typeOfAction = "Multiplicacion"
-      this.result = a *b
-    }else if(type == 'd'){
-      this.typeOfAction = "Division"
-      this.result = a/b
-    }
-
+   numberclick(btn) {
+     
+    if (btn == 'C') {
+      this.result = '';
+  }
+  else if (btn == '=') {
+      this.result = eval(this.result);
+  }
+  else {
+      this.result += btn;
+  }
   }
 
 
