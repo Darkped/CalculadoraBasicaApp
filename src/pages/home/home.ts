3967a3cf514a91d25e2509df7ed4a1a0125d5064
Pedro <ing.pedro.theran@gmail.com>
2018-07-18 12:58:10 -0500

    comint verion 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,25 +7,59 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   
-  result;
+   result:any="";
+   number1:number=0;
+   number2:number=0;
+   op:any=0;
+   total:any="";
   
+
+   
+     
   
 
   constructor(public navCtrl: NavController) {
 
   }
    numberclick(btn) {
-     
-    if (btn == 'C') {
-      this.result = '';
-  }
-  else if (btn == '=') {
-      this.result = eval(this.result);
-  }
-  else {
-      this.result += btn;
-  }
-  }
+       
+            
+        if ((btn == '0') && (this.result.length=='0')) {
+            this.result= "";
+            btn="";
+        }
+        if((btn=='+') || (btn=='-') || (btn=='*') || (btn=='/') ){
+            this.number1=this.result;
+            this.op=btn;
+            this.result="";
+            btn="";
+
+
+        }
+       
+        if (btn == 'C') {
+            this.result= "";
+            
+        }
+            else if (btn == '=') {
+                
+                    this.number2=this.result;
+                    this.result=this.number1+this.op+this.number2;
+                    
+                
+                
+                
+
+                this.result = eval(this.result);
+            }
+ 
+            else {
+                this.result += btn;
+            }
+    }
+
+   
+  
 
 
 }
